d7265b4b9f4ea0f619684f23d6d8bf83b25322f2
Watamage <87782146+Watamage@users.noreply.github.com>
2021-07-24 16:30:54 -0400

    Update holopeek.js

--- holopeek/holopeek.js
+++ holopeek/holopeek.js
@@ -59,8 +59,8 @@
         }
     }, {
         id: 'poll_alert',
-        desc: 'Add a poll sound alert',
-        func: self => pollAlert = !pollAlert
+        desc: 'Remove poll sound alert',
+        func: self => !pollAlert = pollAlert
     }, {
         id: 'reveal_spoilers',
         desc: 'Reveal spoilers',
