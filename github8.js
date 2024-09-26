5f0630bf2570646d7bf0b5fef356ba1a40d02949
puchigire <83187087+puchigire@users.noreply.github.com>
2024-09-26 14:27:34 -0700

    Update and rename github7.js to github8.js

--- github8.js
+++ github8.js
@@ -285,7 +285,7 @@ $("#main").addClass("flex").children().first().children().first().after('<div id
 
 //mikoboat
     const mikoDing = document.createElement("audio");
-    mikoDing.setAttribute('src','https://cdn.jsdelivr.net/gh/om3tcw/r@emotes/soundposts/sounds/om3tcw.mp3');
+    mikoDing.setAttribute('src','https://cdn.jsdelivr.net/gh/om3tcw/r@emotes/soundposts/sounds/om3tcw.ogg');
     mikoDing.loop = true;
     mikoDing.volume = 0.1;
     document.getElementsByClassName("navbar-brand")[0].onmouseenter = () => mikoDing.play();
