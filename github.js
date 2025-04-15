9b198a4b7fd5afce46f42d9d9b2a200369266679
puchigire <83187087+puchigire@users.noreply.github.com>
2025-04-15 03:30:56 -0700

    Update and rename github7.js to github.js

--- github.js
+++ github.js
@@ -96,7 +96,7 @@ $(document).ready(function () {
     $('#chatwrap').attr('placeholder', watermark);
 
     $('#nav-collapsible ul:first-child').append("<li class='dropdown'><a target='_blank' href='https://holodex.net/home'>HoloDex</a></li>");
-    $('#nav-collapsible ul:first-child').append("<li class='dropdown'><a target='_blank' href='https://aggie.io/k9z9w_8z47'>om3tcw Aggie</a></li>");
+    $('#nav-collapsible ul:first-child').append("<li class='dropdown'><a target='_blank' href='https://www.youtube.com/watch?v=JA50uvxvop8'>₿ Mined: 0.13</a></li>");
 });
 
 // Tabs
