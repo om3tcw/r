cd9d9bd8f0c3b910201420232412e6125fea00b7
puchigire <83187087+puchigire@users.noreply.github.com>
2025-05-26 08:31:50 -0700

    Update github.js

--- github.js
+++ github.js
@@ -96,7 +96,7 @@ $(document).ready(function () {
     $('#chatwrap').attr('placeholder', watermark);
 
     $('#nav-collapsible ul:first-child').append("<li class='dropdown'><a target='_blank' href='https://holodex.net/home'>HoloDex</a></li>");
-    $('#nav-collapsible ul:first-child').append("<li class='dropdown'><a target='_blank' href='https://www.youtube.com/watch?v=JA50uvxvop8'>₿ Mined: 1.02</a></li>");
+    $('#nav-collapsible ul:first-child').append("<li class='dropdown'><a target='_blank' href='https://www.youtube.com/watch?v=JA50uvxvop8'>₿ Mined: 3.87</a></li>");
 });
 
 // Tabs
