89d0896225b028617f58c18ecf0e2d2ab53f17e5
puchigire <83187087+puchigire@users.noreply.github.com>
2025-07-24 17:15:45 -0700

    Update github.js

--- github.js
+++ github.js
@@ -164,14 +164,12 @@ $(document).ready(function () {
     };
     redoPollwrap();
 
-    // Teamup
-    $('<div role="tabpanel" class="tab-pane" id="calendarTab"><iframe width="100%" height="600" frameborder="0" scrolling="no"></iframe></div>').appendTo(tabContent);
-    $('<li role="presentation"><a role="tab" data-toggle="tab" aria-expanded="false" href="#calendarTab">Teamup EN</a></li>').appendTo(tabList);
-    const baseCalendarUrl = 'https://teamup.com/ksua2ar4zft49pdn7c?view=m&showLogo=0&showSearch=0&showProfileAndInfo=0&showSidepanel=1&disableSidepanel=0&showTitle=0&showViewSelector=1&showMenu=0&weekStartDay=mo&showAgendaHeader=1&showAgendaDetails=0&showYearViewHeader=1';
+    // oshieyes google
+  
 
-    $('<div role="tabpanel" class="tab-pane" id="calendarTab2"><iframe width="100%" height="600" frameborder="0" scrolling="auto"></iframe></div>').appendTo(tabContent);
-    $('<li role="presentation"><a role="tab" data-toggle="tab" aria-expanded="false" href="#calendarTab2">Oshi Eyes</a></li>').appendTo(tabList);
-    const baseCalendarUrl2 = 'https://docs.google.com/forms/d/1oqO8DIIyxuKVPvhXSAmxNCy5zCkS8XQAhEKi8a9BK1g/viewform?';
+    $('<div role="tabpanel" class="tab-pane" id="calendarTab"><iframe width="100%" height="600" frameborder="0" scrolling="auto"></iframe></div>').appendTo(tabContent);
+    $('<li role="presentation"><a role="tab" data-toggle="tab" aria-expanded="false" href="#calendarTab">Oshi Eyes</a></li>').appendTo(tabList);
+    const baseCalendarUrl = 'https://docs.google.com/forms/d/1oqO8DIIyxuKVPvhXSAmxNCy5zCkS8XQAhEKi8a9BK1g/viewform?';
 
     let calendars = getOrDefault(CHANNEL.name + '_CALENDARS', null);
     if (!Array.isArray(calendars)) {
@@ -182,7 +180,6 @@ $(document).ready(function () {
     };
 
     $('#calendarTab iframe').attr('src', baseCalendarUrl + '&');
-    $('#calendarTab2 iframe').attr('src', baseCalendarUrl2 + '&');
     $('#leftpane').remove();
 }
 
