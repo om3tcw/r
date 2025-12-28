0b88705ff4c08a308fb4d1662b59e4be5c719457
CoNzz97 <connorbar@hotmail.com>
2025-12-28 22:02:55 +0000

    Update customCssInjection.js
    Added a skin to the default playerjs. Should help mods sync up watchalongs

--- custom_modules/custom_css_injection/customCssInjection.js
+++ custom_modules/custom_css_injection/customCssInjection.js
@@ -1,7 +1,8 @@
 const STYLES = {
     CONFETTI_STYLE: `custom_modules/custom_css_injection/confetti-css.js`,
     HOLOPEEK_STYLE: `custom_modules/custom_css_injection/holoPeek-css.js`,
-    SNOW_STYLE: `custom_modules/custom_css_injection/snow-css.js`
+    SNOW_STYLE: `custom_modules/custom_css_injection/snow-css.js`,
+    PLAYER_STYLE: `custom_modules/custom_css_injection/sublime-player-css.js`
 }
 
 $(document).ready(() => {
