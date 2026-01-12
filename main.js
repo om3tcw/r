27723efd675559e7ff049f20d237ec06d36cf38a
Fred <connorbar@hotmail.com>
2026-01-12 02:12:01 +0000

    Update main.js

--- main.js
+++ main.js
@@ -14,10 +14,10 @@ if (![CHANNEL.name].favicon) {
         .appendTo("head");
 }
 
-const LOCAL_CDN_URL = "https://conzz97.github.io/test"; //change before push
+const LOCAL_CDN_URL = "https://immergrok.mikobotecdn.win/immergrok-cytube-fork"; //change before push
 const ONLINE_CDN = "https://mikobotecdn.win";
 //CHANGE WHEN DEVELOPING/LIVE
-const CURRENT_CDN = LOCAL_CDN_URL;
+const CURRENT_CDN = ONLINE_CDN;
 
 const MODULES_FOLDER = "custom_modules/";
 const MODULE_LOADER = `${MODULES_FOLDER}module_orchestration/ModuleLoader.js`;
