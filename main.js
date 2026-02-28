dd203909ef997feea6e8319e1cbf74cc04a229e8
Fred <connorbar@hotmail.com>
2026-02-28 19:36:39 +0000

    Update main.js

--- main.js
+++ main.js
@@ -51,7 +51,7 @@ function makeLiveCDNLink(fileName) {
     return  CURRENT_CDN +
             "/" +
             fileName +
-            "?ver=1-13-9"
+            "?ver=1-13-10"
 }
 
 //candidate to move to util.js
