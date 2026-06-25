8853b2e8f085a8505c5a61ae2367035c28196615
Fred <connorbar@hotmail.com>
2026-06-25 20:52:21 +0100

    Update main.js

--- main.js
+++ main.js
@@ -81,7 +81,7 @@ const ModulePaths = [
 //candidate to move to util.js
 //change ?ver=n to automatically push changes.
 function makeLiveCDNLink(fileName) {
-  return CURRENT_CDN + "/" + fileName + "?ver=1-14-07";
+  return CURRENT_CDN + "/" + fileName + "?ver=1-14-08";
 }
 
 //candidate to move to util.js
