294fd885251356b06024da7f8c95cf27fb02246d
puchigire <83187087+puchigire@users.noreply.github.com>
2026-07-25 14:26:57 -0700

    Update newPoll notification sound URL
    Replaced the notification sound for newPoll.

--- custom_modules/soundNotifications.js
+++ custom_modules/soundNotifications.js
@@ -21,7 +21,7 @@ const NOTIFICATION_DEFAULT_CHOICE_IDS = Object.freeze({
 });
 const NOTIFICATION_BASE_CHOICES = Object.freeze({
   newPoll:
-    "https://cdn.jsdelivr.net/gh/om3tcw/r@emotes/soundposts/sounds/poll.mp3",
+    "https://cdn.jsdelivr.net/gh/om3tcw/r@emotes/soundposts/sounds/ogeyrrat.ogg",
   privateMessage:
     "https://cdn.jsdelivr.net/gh/om3tcw/r@emotes/soundposts/sounds/sharkmail.ogg",
   yourVideoPlays:
