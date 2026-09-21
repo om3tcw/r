2c130b6906bd3d01a739a638b79a5c288dac43b6
CoNzz97 <connorbar@hotmail.com>
2026-09-21 20:47:32 +0100

    Make it so my booky volume sliders wont kill someone

--- main.js
+++ main.js
@@ -82,7 +82,7 @@ const ModulePaths = [
 //candidate to move to util.js
 //change ?ver=n to automatically push changes.
 function makeLiveCDNLink(fileName) {
-  return CURRENT_CDN + "/" + fileName + "?ver=1-15-18";
+  return CURRENT_CDN + "/" + fileName + "?ver=1-15-19";
 }
 
 //candidate to move to util.js
